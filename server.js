6a39e1049d48dcee68ee2768383bb0d1d35c325f
carnellzachary <113737991+carnellzachary@users.noreply.github.com>
2023-10-13 14:22:04 -0400

    MonogDB & Mongoose integration, Model handlers, Error handler

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-const logger = require('./middleware/logger');
+const colors = require('colors');
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
+// const logger = require('./middleware/logger');
+
+// Load env vars found in the config file (before connecting to database)
+dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
-
-// Load env vars found in the config file
-dotenv.config({ path: './config/config.env' });
+// const { connect } = require('mongoose');
 
 // Intialize app with Express
 const app = express();
 
+// Body parser
+app.use(express.json());
+
 // Mount logger
-app.use(logger);
+// app.use(logger);
 
 // Dev logging middleware (for development only)
 if (process.env.NODE_ENV === 'development') {
@@ -23,6 +33,17 @@ if (process.env.NODE_ENV === 'development') {
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
+// Error handiling middleware (has to be executed after bootcamps controller methods)
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+
+// Handle unhandled promise rejections (So server doesn't start if db doesn't connect)
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
