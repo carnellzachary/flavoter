ba0ced7eb6ff399e44b4f33b1ecc40ad0479a9de
carnellzachary <113737991+carnellzachary@users.noreply.github.com>
2023-10-17 01:44:23 -0400

    Resource Ownership Permissions

--- controllers/courses.js
+++ controllers/courses.js
@@ -53,11 +53,17 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 // @access  Private
 exports.addCourse = asyncHandler(async (req, res, next) => {
     req.body.bootcamp = req.params.bootcampId;
+    req.body.user = req.user.id;
 
     const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
     if (!bootcamp) {
         return next(new ErrorResponse(`No bootcamp with id of ${req.params.bootcampId}`), 404);
+    }
+
+    // Make sure user is bootcamp owner
+    if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`User ${req.user.id} is not authorized to add a course to bootcamp ${bootcamp._id}`, 401));
     }
 
     const course = await Course.create(req.body);
@@ -76,6 +82,11 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
 
     if (!course) {
         return next(new ErrorResponse(`No course with id of ${req.params.id}`), 404);
+    }
+
+    // Make sure user is course owner
+    if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`User ${req.user.id} is not authorized to update this course ${course._id}`, 401));
     }
 
     course = await Course.findByIdAndUpdate(req.params.id, req.body, {
@@ -98,6 +109,11 @@ exports.deleteCourse = asyncHandler(async (req, res, next) => {
 
     if (!course) {
         return next(new ErrorResponse(`No course with id of ${req.params.id}`), 404);
+    }
+
+    // Make sure user is course owner
+    if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`User ${req.user.id} is not authorized to delete this course ${course._id}`, 401));
     }
 
     await course.deleteOne();
--- models/Course.js
+++ models/Course.js
@@ -35,6 +35,11 @@ const CourseSchema = new mongoose.Schema({
         type: mongoose.Schema.ObjectId,
         ref: 'Bootcamp', // Which model to reference
         required: true
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User', // Which model to reference
+        required: true
     }
 });
 
